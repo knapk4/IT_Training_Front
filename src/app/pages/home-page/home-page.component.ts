27e6c810fcee297c3ab3a16089d2d7110e349527
zakelh <zakaria.elh.icloud@gmail.com>
2023-08-21 16:29:54 +0200

    integration des partenaires sur la page d'acceuil

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -8,6 +8,19 @@ import { HttpClient } from '@angular/common/http';
 })
 export class HomePageComponent implements OnInit {
   specialites: any[] = [];
+  partners: any[] = [  
+  { name: 'Partner 1', imageUrl: 'assets/url1.jpg' },
+  { name: 'Partner 2', imageUrl: 'assets/url2.jpg' }, 
+  { name: 'Partner 3', imageUrl: 'assets/url3.jpg' },
+  { name: 'Partner 4', imageUrl: 'assets/url4.jpg' },
+  { name: 'Partner 5', imageUrl: 'assets/url5.jpg' },
+  { name: 'Partner 6', imageUrl: 'assets/url6.jpg' },
+  { name: 'Partner 7', imageUrl: 'assets/url7.jpg' },
+  { name: 'Partner 8', imageUrl: 'assets/url8.jpg' },
+  { name: 'Partner 9', imageUrl: 'assets/url9.jpg' },
+
+
+];
 
   constructor(private http: HttpClient) {}
 
