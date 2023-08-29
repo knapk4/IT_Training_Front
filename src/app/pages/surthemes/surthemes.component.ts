1ac9317527e15e4c6d0db76f1f71d76e85200cca
zakelh <zakaria.elh.icloud@gmail.com>
2023-08-29 15:14:52 +0200

    routes quasi fini juste a regler le footer d'ici peu

--- src/app/pages/surthemes/surthemes.component.ts
+++ src/app/pages/surthemes/surthemes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-surthemes',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./surthemes.component.scss']
 })
 export class SurthemesComponent {
+  constructor(private router: Router) {}
 
+  redirectToThemes() {
+    // Rediriger l'utilisateur vers le composant 'themes'
+    this.router.navigate(['/themes']); // Assurez-vous que '/themes' est le bon chemin
+  }
 }
